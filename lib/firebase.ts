8e6deba2b0b21536ebad862ad35b15ab3ec8a94f
mihaescuvlad <52869843+mihaescuvlad@users.noreply.github.com>
2025-04-04 23:31:24 +0300

    Make Firebase Auth functional

--- lib/firebase.ts
+++ lib/firebase.ts
@@ -3,12 +3,15 @@ import { getAuth } from 'firebase/auth';
 import { getDatabase } from 'firebase/database';
 
 const firebaseConfig = {
-    apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
-    authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
-    databaseURL: process.env.NEXT_PUBLIC_FIREBASE_DATABASE_URL,
-    projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
-  };
-  
-  const app = initializeApp(firebaseConfig);
-  export const auth = getAuth(app);
-  export const db = getDatabase(app);
+  apiKey: process.env.NEXT_PUBLIC_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_AUTH_DOMAIN,
+  projectId: process.env.NEXT_PUBLIC_PROJECT_ID,
+  databaseUrl: process.env.NEXT_PUBLIC_DATABASE_URL,
+  messagingSenderId: process.env.NEXT_PUBLIC_MESSAGING_SENDER_ID,
+  appId: process.env.NEXT_PUBLIC_APP_ID,
+  measurementId: process.env.NEXT_PUBLIC_MEASUREMENT_ID,
+};
+
+const app = initializeApp(firebaseConfig);
+export const auth = getAuth(app);
+export const db = getDatabase(app);
