d48759b9ec6a061c97de88b97e9358551dc86cba
mihaescuvlad <52869843+mihaescuvlad@users.noreply.github.com>
2025-04-04 21:12:39 +0300

    Add Pwsh 7 Compatibility for .env

---
+++ bin/dotenv-decrypt-encrypt.ps1
@@ -0,0 +1,33 @@
+param(
+    [Parameter(Position = 0)]
+    [ValidateSet("encrypt", "decrypt")]
+    [string]$Action
+)
+
+Set-Location (Split-Path -Parent $MyInvocation.MyCommand.Path)
+
+$encrypt = if ([string]::IsNullOrEmpty($Action)) { 
+    $false 
+} elseif ($Action -eq "encrypt") { 
+    $true 
+} else { 
+    $false 
+}
+
+$securePassword = Read-Host -AsSecureString -Prompt "Enter password for $($encrypt ? 'encryption' : 'decryption')"
+$BSTR = [System.Runtime.InteropServices.Marshal]::SecureStringToBSTR($securePassword)
+$password = [System.Runtime.InteropServices.Marshal]::PtrToStringAuto($BSTR)
+
+try {
+    if ($encrypt) {
+        $password | openssl aes-256-cbc -a -salt -pbkdf2 -in ../.env -out ../.env.enc -pass stdin
+    } else {
+        $password | openssl aes-256-cbc -d -a -salt -pbkdf2 -in ../.env.enc -out ../.env -pass stdin
+    }
+} catch {
+    Write-Error "Error occurred during $($encrypt ? 'encryption' : 'decryption'): $_"
+    exit 1
+} finally {
+    $password = $null
+    [System.Runtime.InteropServices.Marshal]::ZeroFreeBSTR($BSTR)
+}
