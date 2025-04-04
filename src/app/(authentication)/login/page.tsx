6b958a9c045181b4780e4445545bcabc429f55eb
mihaescuvlad <52869843+mihaescuvlad@users.noreply.github.com>
2025-04-04 23:31:24 +0300

    Make Firebase Auth functional

--- src/app/(authentication)/login/page.tsx
+++ src/app/(authentication)/login/page.tsx
@@ -1,6 +1,35 @@
-export default async function Page() {
+"use client";
+import { useState } from "react";
+
+import { signInWithEmailAndPassword } from "firebase/auth";
+import { auth } from "../../../../lib/firebase";
+
+export default function Page() {
+    const [error, setError] = useState("");
+    const [visible, setVisible] = useState(false);
+
+    const toggleVisible = () => {
+        setVisible((previusVisible) => !previusVisible);
+    };
+  
+    const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
+      e.preventDefault();
+      const form = e.target as HTMLFormElement;
+      const email = form.email.value;
+      const password = form.password.value;
+  
+      try {
+        await signInWithEmailAndPassword(auth, email, password);
+        console.log("Signed in!");
+      } catch (err) {
+        if (err instanceof Error) {
+          setError(err.message);
+        }
+      }
+    };
+
     return (
-        <form className="shadow-lg rounded-md border border-neutral/10 p-10 w-full max-w-100">
+        <form onSubmit={handleSubmit} className="shadow-lg rounded-md border border-neutral/10 p-10 w-full max-w-100">
             <div className="text-center mb-5">
                 <h2 className="text-xl font-semibold">
                     OfCoursly
@@ -14,12 +43,37 @@ export default async function Page() {
             </h3>
             <fieldset className="fieldset">
                 <legend className="fieldset-legend">Email</legend>
-                <input type="text" className="input" placeholder="johndoe@example.com" />
+                <input name="email" type="text" className="input input-bordered w-full" placeholder="johndoe@example.com" />
             </fieldset>
             <fieldset className="fieldset">
-                <legend className="fieldset-legend">Password</legend>
-                <input type="text" className="input" placeholder="very secret password" />
+                <label className="label">
+                <span className="label-text">Password</span>
+                </label>
+                <div className="relative">
+                <input
+                    name="password"
+                    type={visible ? "text" : "password"}
+                    placeholder="Enter your password"
+                    className="input input-bordered w-full pr-12"
+                    required
+                />
+                <button
+                    type="button"
+                    className="absolute right-3 top-1/2 -translate-y-1/2 text-sm text-gray-500"
+                    onClick={toggleVisible}
+                >
+                    {visible ? "Hide" : "Show"}
+                </button>
+                </div>
+                {error && (
+                <label className="label">
+                    <span className="label-text-alt text-error">{error}</span>
+                </label>
+                )}
             </fieldset>
+            <div className="mt-6">
+                <button className="btn btn-primary w-full">Sign In</button>
+            </div>
         </form>
     )
 }
