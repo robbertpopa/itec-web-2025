44a1920d19376d991b92de248621bd2f6d757d90
atomicul <soaremihai2006@gmail.com>
2025-04-05 21:29:24 +0300

    Add file viewing

--- src/app/(authenticated)/courses/[courseId]/[lessonId]/[[...path]]/page.tsx
+++ src/app/(authenticated)/courses/[courseId]/[lessonId]/[[...path]]/page.tsx
@@ -1,7 +1,19 @@
 import { storage } from "lib/firebase";
 import { ref, getMetadata, getDownloadURL } from "firebase/storage"
-import { notFound, redirect } from "next/navigation";
+import { notFound, redirect, RedirectType } from "next/navigation";
 import { NextResponse } from "next/server";
+import { firebase } from "lib/firebaseServer";
+
+interface Props {
+    markdown?: string;
+    children?: BucketFile[];
+}
+
+interface BucketFile {
+    name: string
+    mimeType?: string
+    size: string
+}
 
 interface ParamsType {
     courseId: string
@@ -9,33 +21,78 @@ interface ParamsType {
     path?: string[]
 }
 
-export default async function Page({ params }:
-    { params: Promise<ParamsType> }) {
+export default async function Page({ params }: { params: Promise<ParamsType> }) {
+    const {
+        courseId,
+        lessonId,
+        path
+    } = await params;
+
+    const bucket = firebase().storage().bucket();
+    const fullPath = `courses/${courseId}/${lessonId}${path ? '/' + path.join('/') : ''}`
+
+    const [files, ,apiResponse] = await bucket.getFiles({ prefix: fullPath + '/', delimiter: '/', autoPaginate: true })
+
+    const prefixes = ((apiResponse as any).prefixes  ?? []) as any[]
+
+    if (files.length + prefixes.length > 1) {
+        files.shift();
+
+        const mainFile = files.find(f => f.name == "main.md");
+        const markdown = (await mainFile?.download())?.toString();
+
+        const bucketFiles = files.map(f => ({
+            name: f.name.split('/').filter(x => x != "").at(-1),
+            mimeType: f.metadata.contentType == "application/x-www-form-urlencoded;charset=UTF-8" ? null : f.metadata.contentType,
+            size: (f.metadata.size ?? 0).toString(),
+        } as BucketFile))
+
+        const prefixFiles = prefixes.map(p => ({
+            name: p.split('/').filter(x => x != "").at(-1),
+            size: '0',
+        } as BucketFile))
+        
+        return render({
+            markdown,
+            children: [...prefixFiles, ...bucketFiles],
+        });
+    }
+
+    let file = files.length == 1 ? files[0] : null;
+
+    if (!file && (await bucket.file(fullPath).exists())[0]) {
+        file = bucket.file(fullPath);
+    }
+
+    if (file) {
+        if (file.metadata?.contentType == "text/markdown") {
+            const [markdown] = await files[0].download();
+            return render({
+                markdown: markdown.toString("utf8")
+            });
+        }
+
+        const [url] = await file.getSignedUrl(
+            {
+                action: "read",
+                expires: Date.now() + 15 * 60 * 1000
+            });
 
 
-    return <h1>Whyyy</h1>
+        redirect(url, RedirectType.replace);
+    }
+
+
+    notFound();
 }
 
-export const dynamic = "force-dynamic";
-
-// export async function getServerSideProps({ params }:
-//     { params: Promise<ParamsType> }) {
-//
-//     const {
-//         courseId,
-//         lessonId,
-//         path
-//     } = await params;
-//
-//     const storage = getStorage(firebase);
-//     const bucket = storage.bucket();
-//     const file = bucket.file(`${courseId}/${lessonId}/${path?.join('/') ?? ''}`);
-//     if (!file.exists) {
-//         console.log("I don't exist bro");
-//         return {};
-//     }
-//
-//     console.log(file.publicUrl());
-//     const redirects = {}
-//
-// }
+function render(props: Props) {
+    return <>
+        {props.markdown && <p>{props.markdown}</p>}
+        {props.children && props.children.map(f => {
+            return <div key={f.name}>
+                <span>{f.name} {f.mimeType ?? "folder"} {f.size}</span>
+            </div>
+        })}
+    </>
+}
