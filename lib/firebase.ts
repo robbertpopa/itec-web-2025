2ae9045115eb572436a614e441088837c72ec362
atomicul <soaremihai2006@gmail.com>
2025-04-05 14:09:18 +0300

    Add firebase server side

--- lib/firebase.ts
+++ lib/firebase.ts
@@ -3,20 +3,21 @@ import { getAuth } from 'firebase/auth';
 import { GoogleAuthProvider } from "firebase/auth";
 import { getDatabase } from 'firebase/database';
 import { getStorage } from 'firebase/storage';
-import { isSupported, getAnalytics } from 'firebase/analytics';
+import admin from 'firebase-admin';
+import serviceAccount from '.firebase-service-account.json';
 
 const firebaseConfig = {
-  apiKey: process.env.NEXT_PUBLIC_API_KEY,
-  projectId: process.env.NEXT_PUBLIC_PROJECT_ID,
-  appId: process.env.NEXT_PUBLIC_APP_ID,
+    apiKey: process.env.NEXT_PUBLIC_API_KEY,
+    projectId: process.env.NEXT_PUBLIC_PROJECT_ID,
+    appId: process.env.NEXT_PUBLIC_APP_ID,
 
-  authDomain: process.env.NEXT_PUBLIC_AUTH_DOMAIN,
-  messagingSenderId: process.env.NEXT_PUBLIC_MESSAGING_SENDER_ID,
+    authDomain: process.env.NEXT_PUBLIC_AUTH_DOMAIN,
+    messagingSenderId: process.env.NEXT_PUBLIC_MESSAGING_SENDER_ID,
 
-  databaseURL: process.env.NEXT_PUBLIC_DATABASE_URL,
-  storageBucket: process.env.NEXT_PUBLIC_STORAGE_BUCKET,
-  
-  measurementId: process.env.NEXT_PUBLIC_MEASUREMENT_ID,
+    databaseURL: process.env.NEXT_PUBLIC_DATABASE_URL,
+    storageBucket: process.env.NEXT_PUBLIC_STORAGE_BUCKET,
+
+    measurementId: process.env.NEXT_PUBLIC_MEASUREMENT_ID,
 };
 
 const app = initializeApp(firebaseConfig);
@@ -25,6 +26,7 @@ export const googleProvider = new GoogleAuthProvider();
 export const db = getDatabase(app);
 export const storage = getStorage(app);
 
-// export const analytics = await isSupported().then(
-//   (supported) => supported ? getAnalytics(app) : null
-// );
+export const firebase = admin.initializeApp({
+    ...firebaseConfig,
+    credential: admin.credential.cert(JSON.stringify(serviceAccount)),
+});
